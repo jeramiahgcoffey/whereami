a77b57737039d14f568bea4aeb5a59b3f94bb346
Jeramiah <jeramiahcoffey.dev@gmail.com>
2023-03-08 16:48:51 -0600

    implment hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/root';
 import ThemeProvider from './context/providers';
 import Home from './pages/Home';
@@ -8,31 +8,28 @@ import './index.css';
 import Details from './pages/Details';
 import { getCountries, getCountry } from './api';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Root />,
-      children: [
-        {
-          path: '/',
-          element: <Home />,
-          loader: async () => {
-            return await getCountries();
-          },
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+        loader: async () => {
+          return await getCountries();
         },
-        {
-          path: '/:alpha3Code',
-          element: <Details />,
-          loader: async ({ params }) => {
-            return await getCountry(params.alpha3Code!);
-          },
+      },
+      {
+        path: '/:alpha3Code',
+        element: <Details />,
+        loader: async ({ params }) => {
+          return await getCountry(params.alpha3Code!);
         },
-      ],
-    },
-  ],
-  { basename: '/whereami' }
-);
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
